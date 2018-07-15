dba1dd6b2f747d581dfd05e53213bb565436a472
debao.fu <debao.fu@huilianyi.com>
2018-07-15 21:39:37 +0800

    fixed

--- config/index.js
+++ config/index.js
@@ -21,11 +21,11 @@ module.exports = {
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
     proxyTable: {
-      'api': {
+      '/api': {
         target: 'http://47.104.99.233:8083',
         secure: true,
         changeOrigin: true,
-        pathRewrite: { '^api': '' }
+        pathRewrite: { '^/api': '' }
       }
     },
     /**
